8039b6ccfc27acd9a0c3df90f64f4f9af39e4dad
Popescu Alexandru <alexpopescu021@gmail.com>
2024-05-17 00:06:34 +0300

    try 4 candlestick

--- src/app/coin-detail/coin-detail.component.ts
+++ src/app/coin-detail/coin-detail.component.ts
@@ -1,10 +1,19 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { ChartConfiguration, ChartType } from 'chart.js';
-import 'chartjs-chart-financial';
-import { BaseChartDirective } from 'ng2-charts';
+import { Chart, LinearScale, TimeScale } from 'chart.js';
+import {
+  CandlestickController,
+  CandlestickElement,
+} from 'chartjs-chart-financial';
 import { ApiService } from '../services/externalServices/service/api.service';
 import { ExtCurrencyService } from '../services/externalServices/service/ext-currency.service';
+
+Chart.register(
+  CandlestickElement,
+  CandlestickController,
+  TimeScale,
+  LinearScale
+);
 
 @Component({
   selector: 'app-coin-detail',
@@ -12,38 +21,12 @@ import { ExtCurrencyService } from '../services/externalServices/service/ext-cur
   styleUrls: ['./coin-detail.component.scss'],
 })
 export class CoinDetailComponent implements OnInit {
+  @ViewChild('chart') chartRef!: ElementRef;
+  chart!: Chart;
   coinData: any;
   coinId!: string;
   days: number = 30;
   currency: string = 'EUR';
-  public lineChartData: ChartConfiguration['data'] = {
-    datasets: [
-      {
-        data: [],
-        label: `Price Trends`,
-        backgroundColor: 'rgba(148,159,177,0.2)',
-        borderColor: '#009688',
-        pointBackgroundColor: '#009688',
-        pointBorderColor: '#009688',
-        pointHoverBackgroundColor: '#009688',
-        pointHoverBorderColor: '#009688',
-      },
-    ],
-    labels: [],
-  };
-  public lineChartOptions: ChartConfiguration['options'] = {
-    elements: {
-      point: {
-        radius: 1,
-      },
-    },
-
-    plugins: {
-      legend: { display: true },
-    },
-  };
-  public lineChartType: ChartType = 'candlestick';
-  @ViewChild(BaseChartDirective) myLineChart!: BaseChartDirective;
 
   constructor(
     private api: ApiService,
@@ -76,22 +59,37 @@ export class CoinDetailComponent implements OnInit {
       this.coinData = res;
     });
   }
+
   getGraphData(days: number) {
     this.days = days;
     this.api
       .getGrpahicalCurrencyData(this.coinId, this.currency, this.days)
       .subscribe((res) => {
-        setTimeout(() => {
-          this.myLineChart.chart?.update();
-        }, 200);
-        this.lineChartData.datasets[0].data = res.map((a: any) => {
-          return {
-            t: a[0], // time
-            o: a[1], // open
-            h: a[2], // high
-            l: a[3], // low
-            c: a[4], // close
-          };
+        this.chart = new Chart(this.chartRef.nativeElement, {
+          type: 'candlestick',
+          data: {
+            datasets: [
+              {
+                data: res.map((a: any) => ({
+                  t: a[0], // time
+                  o: a[1], // open
+                  h: a[2], // high
+                  l: a[3], // low
+                  c: a[4], // close
+                })),
+              },
+            ],
+          },
+          options: {
+            scales: {
+              x: {
+                type: 'time',
+              },
+              y: {
+                type: 'linear',
+              },
+            },
+          },
         });
       });
   }
